0e1896fc028d9a78dd4f7d972f0b1fde3f33db2b
Randhall Rangelrraandha <randhall_rd@hotmail.com>
2018-09-06 17:34:31 -0300

    Se agregan estilos a los botones

--- AgenciaMedica/AgenciaMedica/Controllers/ActionController.cs
+++ AgenciaMedica/AgenciaMedica/Controllers/ActionController.cs
@@ -24,14 +24,20 @@ namespace AgenciaMedica.Controllers
         [HttpPost]
         public ActionResult Ingresar(string userName, string email, string password)
         {
-            Usuario unUsuario = new Usuario();
-
-            unUsuario.User = userName;
-            unUsuario.Password = password;
+            Usuario unUsuario = new Usuario
+            {
+                User = userName,
+                Password = password
+            };
             //Crear todo el esquema de ingreso.
 
             return View("Registro");
+        }
 
+        [HttpPost]
+        public ActionResult Registrar(string userName, string email, string password)
+        {
+            return View("Registro");
         }
     }
 }
--- AgenciaMedica/AgenciaMedica/Models/Usuario.cs
+++ AgenciaMedica/AgenciaMedica/Models/Usuario.cs
@@ -10,8 +10,11 @@ namespace AgenciaMedica.Models
         int idUsuario;
         string user;
         string password;
+        string email;
+        
         public int IdUsuario { get => idUsuario; set => idUsuario = value; }
         public string User { get => user; set => user = value; }
         public string Password { get => password; set => password = value; }
+        public string Email { get => email; set => email = value; }
     }
 }
